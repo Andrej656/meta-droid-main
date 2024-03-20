420700ce7f3b8d89882112a32d6c153d801e7dfe
Btcdev <chaindev@icloud.com>
2024-03-20 21:00:23 +0100

    bug fix 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -24,14 +24,12 @@ const Footer = () => (
             className="w-[24px] h-[24px] object-contain"
           />
           <span className="font-normal text-[16px] text-white">
-               Udji mala u moju sobu
+            Udji mala u moju sobu
           </span>
         </button>
       </div>
- 
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">
             METADROID
@@ -39,7 +37,6 @@ const Footer = () => (
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metadroid. All rights reserved.
           </p>
-
           <div className="flex gap-4">
             {socials.map((social) => (
               <a key={social.name} href={social.link}>
